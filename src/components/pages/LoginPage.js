b16d223db19fd934d33d3a6bd8444d58a7f133d9
chaewon127 <osobi621@gmail.com>
2025-09-24 18:45:53 +0900

    실습 완료

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 import Label from "../Label";
 import Input from "../Input";
 import Button from "../Button";
@@ -13,6 +14,9 @@ function LoginPage() {
     email: "",
     password: "",
   });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const router = useRouter();
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -25,12 +29,58 @@ function LoginPage() {
 
   async function handleSubmit(e) {
     e.preventDefault();
+
+    const { email, password } = values;
+
+    if (!email || !password) {
+      setError("모든 필드를 입력해주세요.");
+      return;
+    }
+
+    try {
+      setLoading(true);
+      setError(null);
+
+      const res = await fetch(
+        "https://learn.codeit.kr/api/link-service/auth/login",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+          body: JSON.stringify({
+            email: values.email,
+            password: values.password,
+          }),
+        }
+      );
+
+      if (!res.ok) {
+        throw new Error("로그인 실패");
+      }
+
+      let data = null;
+      try {
+        data = await res.json();
+      } catch (err) {
+        console.warn("JSON 응답이 비어있습니다");
+      }
+      console.log("로그인 성공", data);
+
+      router.push("/me");
+
+      return data;
+    } catch (error) {
+      console.error(error);
+      setError("로그인에 실패했습니다.");
+    } finally {
+      setLoading(false);
+    }
     // TODO: 로그인 처리
-    // 1. fetch 를 사용하여 로그인 요청을 보냅니다.
-    // 2. 성공 시 Application 에 쿠키 내 토큰 저장 여부를 확인합니다.
-    // 3. 로딩 상태를 만들고 로딩중일 때는 로그인 버튼을 비활성화 합니다.
-    // 4. 추가로 로딩중일 때는 로그인 버튼텍스트를 "로그인 중..."으로 변경합니다.
-    // 5. 에러 상태를 만들고 로그인 요청이 실패 시 에러 메시지를 로그인버튼 바로 위에 표시합니다.
+    // 1. fetch 를 사용하여 로그인 요청을 보냅니다. v
+    // 2. 성공 시 Application 에 쿠키 내 토큰 저장 여부를 확인합니다. v
+    // 3. 로딩 상태를 만들고 로딩중일 때는 로그인 버튼을 비활성화 합니다. v
+    // 4. 추가로 로딩중일 때는 로그인 버튼텍스트를 "로그인 중..."으로 변경합니다. v
+    // 5. 에러 상태를 만들고 로그인 요청이 실패 시 에러 메시지를 로그인버튼 바로 위에 표시합니다. v
   }
 
   return (
@@ -61,7 +111,10 @@ function LoginPage() {
           value={values.password}
           onChange={handleChange}
         />
-        <Button className={styles.Button}>로그인</Button>
+        {error && <div className={styles.Error}>{error}</div>}
+        <Button className={styles.Button} type="submit" disabled={loading}>
+          {loading ? "로그인 중…" : "로그인"}
+        </Button>
         <HorizontalRule className={styles.HorizontalRule}>또는</HorizontalRule>
         <Button
           className={styles.GoogleButton}
